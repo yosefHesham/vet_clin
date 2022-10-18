f87914f17493a91ce5548045bef5a27ee6871a9f
yosefHesham <youssef.h@trufla.com>
2022-10-18 22:40:25 +0200

    Use aggregation

--- data.sql
+++ data.sql
@@ -1,7 +1,3 @@
-Animal: His name is Agumon. He was born on Feb 3rd, 2020, and currently weighs 10.23kg. He was neutered and he has never tried to escape.
-Animal: Her name is Gabumon. She was born on Nov 15th, 2018, and currently weighs 8kg. She is neutered and she has tried to escape 2 times.
-Animal: His name is Pikachu. He was born on Jan 7th, 2021, and currently weighs 15.04kg. He was not neutered and he has tried to escape once.
-Animal: Her name is Devimon. She was born on May 12th, 2017, and currently weighs 11kg. She is neutered and she has tried to escape 5 times.
 
 insert into animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
  values 
@@ -9,4 +5,17 @@ insert into animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
 ('Gabumon','Nov 26, 2018',8,true,2),
 ('Pikachu','Jan 7, 2021',15.04,false,1),
 ('Devimon','May 12, 2017',11,true,5)
+insert into animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
+ values 
+('Charmander','Feb 2, 2020',-11,false,0),
+('Plantmon','Nov 15, 2021',-5.7,true,2),
+('Squirtle','Apr 2, 1993',-12.13,false,3),
+('Angemon','Jun 12, 2005',-45,true,1),
+('Boarmon','Jun 7, 2005',20.4,true,7),
+('Blossom','Oct 13, 1998',17,true,3),
+('Ditto','May 14, 2022',22,true,4)
+
+
+
+
 
--- queries.sql
+++ queries.sql
@@ -7,3 +7,39 @@ Select name, escape_attempts from animals where weight_kg > 10.5;
 select * from animals where neutered
 select * from animals where name != 'Gabumon';
 Select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
+
+Begin;
+update animals set species = 'unspecified';
+update animals set species = 'pokemon';
+update animals set species = 'digimon' where name like '%mon';
+commit;
+
+select * from animals;
+Begin;
+savepoint mysavepoint;
+delete from animals;
+rollback to savepoint mysavepoint;
+commit;
+select * from animals;
+
+How many animals are there?
+How many animals have never tried to escape?
+What is the average weight of animals?
+Who escapes the most, neutered or not neutered animals?
+What is the minimum and maximum weight of each type of animal?
+What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+select count(*) from animals;
+
+select count(*) from animals where escape_attempts = 0;
+
+select avg(weight_kg) from animals;
+
+select neutered, count(escape_attempts) from animals group by neutered;
+
+select species, min(weight_kg) as min, max(weight_kg) as max  from animals group by species;
+
+select species, avg(escape_attempts) from animals where date_of_birth between '1990-01-01' and '2000-12-31' group by species ;
+
+
+
