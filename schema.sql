8ba8791429d24bd9ba8afb1d00f3b0da72dd6e22
yosefHesham <youssef.h@trufla.com>
2022-10-19 22:42:34 +0200

    Create tables with data

--- schema.sql
+++ schema.sql
@@ -9,3 +9,18 @@ create table animals (
 )
 
 alter table animals add species varchar(50)
+
+create table owners (
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(50),
+    age int 
+);
+
+create table species(
+    id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(50)
+);
+
+alter table animals add species_id int references species(id);
+alter table animals add owner_id int references owners(id);
+
